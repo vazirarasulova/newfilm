704526fc2948b061a2d8070bb70d84eeaaa73a1e
vazirarasulova <rasulovavazira20@gmail.com>
2022-07-04 09:06:25 +0500

    Add sort

--- js/main.js
+++ js/main.js
@@ -1,53 +1,71 @@
-var elList = document.querySelector(".film__list");
-var elForm = document.querySelector(".form");
-var elSelect = document.querySelector(".js-select");
+let elList = document.querySelector(".film__list");
+let elForm = document.querySelector(".form");
+let elSelect = document.querySelector(".js-select");
+let elSort = document.querySelector(".js-sort");
 
+// const sortFunction = {
+//   az: (a, b) => {
+//     if (a.title.toLowerCase() < b.title.toLowerCase()) {
+//       return -1
+//     } else {
+//       return 1
+//     }
+//   },
+//   za: (a, b) => {
+//     if (a.title.toLowerCase() < b.title.toLowerCase()) {
+//       return 1
+//     } else {
+//       return -1
+//     }
+//   }
+// }
 
 function renderType(arr, element) {
   let renderTypes = [];
 
   arr.forEach((f) => {
-    f.genres.forEach(genres => {
+    f.genres.forEach((genres) => {
       if (!renderTypes.includes(genres)) {
-        renderTypes.push(genres)
+        renderTypes.push(genres);
       }
-    })
-  })
+    });
+  });
 
-  renderTypes.forEach(genres => {
+  renderTypes.forEach((genres) => {
     const newOption = document.createElement("option");
     newOption.value = genres;
     newOption.textContent = genres;
     element.appendChild(newOption);
-  })
+  });
 }
 
 function func(arr) {
-  elList.innerHTML = ""
+  elList.innerHTML = "";
 
-  for (var film of arr) {
-
-    var newItem = document.createElement("li");
-    var newImg = document.createElement("img");
-    var newSubbox = document.createElement("div")
-    var newHeading = document.createElement("h2");
-    var newText = document.createElement("p");
-    var newTime = document.createElement("time");
-    var newSublist = document.createElement("ul");
-    var newGenres = document.createElement("li");
-
+  for (let film of arr) {
+    let newItem = document.createElement("li");
+    let newImg = document.createElement("img");
+    let newSubbox = document.createElement("div");
+    let newHeading = document.createElement("h2");
+    let newText = document.createElement("p");
+    let newTime = document.createElement("time");
+    let newSublist = document.createElement("ul");
+    let newGenres = document.createElement("li");
 
     newHeading.textContent = film.title;
-    newText.textContent = film.overview.split(" ").slice(0, 10).join(" ") + "...";
+    newText.textContent =
+      film.overview.split(" ").slice(0, 10).join(" ") + "...";
     newGenres.textContent = film.genres.join(", ");
-
 
     newItem.setAttribute("class", "card m-3 bg-secondary");
     newItem.setAttribute("style", "width: 18rem");
     newImg.setAttribute("src", film.img);
     newImg.setAttribute("height", 280);
     newImg.setAttribute("class", "card-img");
-    newSubbox.setAttribute("class", "card-body text-center bg-secondary bg-opacity-25");
+    newSubbox.setAttribute(
+      "class",
+      "card-body text-center bg-secondary bg-opacity-25"
+    );
     newText.setAttribute("class", "card-title");
     newText.setAttribute("class", "card-text");
     newTime.setAttribute("class", "card-time");
@@ -58,30 +76,53 @@ function func(arr) {
     newTime.setAttribute("datetime", "2022-03-12");
     newSublist.setAttribute("class", "card-list list-unstyled");
 
-
     newItem.appendChild(newImg);
-    newItem.appendChild(newSubbox)
+    newItem.appendChild(newSubbox);
     newSubbox.appendChild(newHeading);
     newSubbox.appendChild(newText);
     newSubbox.appendChild(newTime);
     newSubbox.appendChild(newSublist);
     newSublist.appendChild(newGenres);
     elList.appendChild(newItem);
-
   }
 }
 
-elForm.addEventListener("change", evt => {
+elSelect.addEventListener("change", (evt) => {
   evt.preventDefault();
 
   const elSelectVal = elSelect.value;
 
-  let filterTypes = elSelectVal == "all genres" ? films : films.filter(f => f.genres.includes(elSelectVal));
+  let filterTypes =
+    elSelectVal == "all genres"
+      ? films
+      : films.filter((f) => f.genres.includes(elSelectVal));
 
-  console.log(filterTypes);
   func(filterTypes, elList);
-})
+});
 
+elSort.addEventListener("change", function () {
+  let elva = elSort.value;
+
+  let sortedFilms = films.sort((a, b) => {
+    if (elva === "az") {
+      if (a.title > b.title) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else {
+      if (a.title < b.title) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  });
+
+  func(sortedFilms);
+  console.log(elva);
+  console.log(sortedFilms);
+});
 
 func(films);
 renderType(films, elSelect);
